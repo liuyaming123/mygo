4ab42d2140502f89860bc8c3a575af3e7a9bafdc
liuyaming <lym_6017@163.com>
2021-07-01 15:16:50 +0000

    array

--- gotest/array_test.go
+++ gotest/array_test.go
@@ -34,8 +34,8 @@ func TestArray(t *testing.T) {
 	// IndexValue2(a1, 8)
 
 	a2 := []int{}
-	// fmt.Println(IndexValue1(GenSlice(a2), 100))
-	fmt.Println(IndexValue2(GenSlice(a2), 100))
+	fmt.Println(IndexValue1(GenSlice(a2), 100))
+	// fmt.Println(IndexValue2(GenSlice(a2), 100))
 
 }
 
@@ -66,8 +66,7 @@ func sumArr(n [10]int) int {
 
 // 找出数组中和为给定值的两个元素的下标，例如数组[1,3,5,8,7]，
 // 找出两个元素之和等于8的下标分别是（0，4）和（1，2）
-func IndexValue1(a []int, target int) int {
-	cnt := 0
+func IndexValue1(a []int, target int) (cnt int) {
 	for i := 0; i < len(a); i++ {
 		other := target - a[i]
 		for j := i + 1; j < len(a); j++ {
@@ -78,11 +77,10 @@ func IndexValue1(a []int, target int) int {
 			}
 		}
 	}
-	return cnt
+	return
 }
 
-func IndexValue2(a []int, target int) int {
-	cnt := 0
+func IndexValue2(a []int, target int) (cnt int) {
 	m := make(map[int]int)
 	for i := 0; i < len(a); i++ {
 		other := target - a[i]
@@ -97,7 +95,7 @@ func IndexValue2(a []int, target int) int {
 			cnt++
 		}
 	}
-	return cnt
+	return
 }
 
 // 生成1000以内的随机整数并添加到slice
