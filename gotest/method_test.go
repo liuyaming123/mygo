144be0459a1d6fa804d7baab0ec8889408a3a749
liuyaming <lym_6017@163.com>
2021-11-25 13:01:51 +0000

    method接收器为指针类型和不是指针类型的区别

--- gotest/method_test.go
+++ gotest/method_test.go
@@ -34,6 +34,11 @@ func Distance(p, q Point) float64 {
 }
 
 // same thing, bug as a method of the Point type
+/*
+func (p *Point) Distance(q Point){}  && func (p Point) Distance(q Point) float64 {}
+不管你的method的receiver是指针类型还是非指针类型，都是可以通过指针/非指针类型进行调用的，编译器会帮你做类型转换。
+在声明一个method的receiver该是指针还是非指针类型时，你需要考虑两方面的因素，第一方面是这个对象本身是不是特别大，如果声明为非指针变量时，调用会产生一次拷贝；第二方面是如果你用指针类型作为receiver，那么你一定要注意，这种指针类型指向的始终是一块内存地址，就算你对其进行了拷贝。熟悉C或者C++的人这里应该很快能明白。
+*/
 func (p Point) Distance(q Point) float64 {
 	return math.Hypot(p.X-q.X, p.Y-q.Y)
 }
