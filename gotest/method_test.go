372e9d857bc37ad500a3184d7633c655113fdf97
liuyaming <lym_6017@163.com>
2021-11-25 12:45:28 +0000

    method

--- gotest/method_test.go
+++ gotest/method_test.go
@@ -10,6 +10,24 @@ type Point struct {
 	X, Y float64
 }
 
+type Path []Point
+
+/*
+我们可以给同一个包内的任意命名类型定义方法，只要这个命名类型的底层类型
+（译注：这个例子里，底层类型是指[]Point这个slice，Path就是命名类型）
+不是指针或者interface
+*/
+
+func (path Path) Distance() float64 {
+	sum := 0.0
+	for i := range path {
+		if i > 0 {
+			sum += path[i-1].Distance(path[i])
+		}
+	}
+	return sum
+}
+
 // traditional function
 func Distance(p, q Point) float64 {
 	return math.Hypot(q.X-p.X, q.Y-p.Y)
@@ -25,4 +43,7 @@ func TestMethod(t *testing.T) {
 	p2 := Point{4, 5}
 
 	fmt.Printf("%#v\n", p1.Distance(p2))
+
+	p3 := Path{{0, 0}, {0, 1}, {0, 5}, {0, 3}}
+	fmt.Println(p3.Distance())
 }
